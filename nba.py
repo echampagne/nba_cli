5c45a9da0a8a23c1e77b1eca22fa3a6b49faf7fb
echampagne <echampagne27@gmail.com>
2015-09-02 20:18:28 -0400

    format output

--- nba.py
+++ nba.py
@@ -28,26 +28,43 @@ def get_standings(month=TODAY.month,
 	res_json = get_json(endpoint='scoreboard', params={'LeagueID': league_id,
 													'GameDate': game_date,
 													'DayOffset': offset})
-	# print json.dumps(res_json['resultSets'][4]['rowSet'], indent=2)
-	# print json.dumps(res_json['resultSets'][4]['headers'], indent=2)
 	if conference.lower() == 'east':
 		print_standings(res_json['resultSets'][4]['rowSet'])
 	else:
 		print_standings(res_json['resultSets'][5]['rowSet'])
 
 def print_standings(standings):
-	for team in standings:
-		print "Team: %s \t W: %s \t L: %s" % (team[5], team[7], team[8])
+	# Sort by win percentage to display
+	standings.sort(key=lambda x: x[9], reverse=True)
+	click.secho("{pos:6} {team:15} {wins:10} {losses:10} {winpercent:10}".format(
+			pos="#", team="TEAM", wins="WINS", losses="LOSSES", winpercent="WIN PCT"
+		), bold=True)
+	for index, team in enumerate(standings, start=1):
+		# secho is styled echo, fg='green' for example
+		if index < 9:
+			click.secho("{pos:6} {team:15} {wins:10} {losses:10} {winpercent:10}".format(
+				pos=str(index), team=str(team[5]), wins=str(team[7]), losses=str(team[8]), winpercent=str(team[9])
+			), fg="green")
+		else:
+			click.secho("{pos:6} {team:15} {wins:10} {losses:10} {winpercent:10}".format(
+				pos=str(index), team=str(team[5]), wins=str(team[7]), losses=str(team[8]), winpercent=str(team[9])
+			), fg="blue")
+		# print "Team: %s \t W: %s \t L: %s" % (team[5], team[7], team[8])
 
+
+# Set up command line arguments
 @click.command()
-@click.option('--standings', is_flag=True, help="Standing for a conference")
-@click.option('--conference', type=click.Choice(['East', 'West', 'east', 'west']),
+@click.option('--standings', '-s', is_flag=True, help="Standing for a conference")
+@click.option('--conference', '-c', type=click.Choice(['East', 'West', 'east', 'west']),
 	help = (
 		"Choose a conference, East or West"
 	)
 )
 
 def main(standings, conference):
+	'''
+	Command line interface for the NBA
+	'''
 	if standings and conference:
 		get_standings(conference=conference)
 		return;
